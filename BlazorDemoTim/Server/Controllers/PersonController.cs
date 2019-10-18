c6ca5d3d8dc9443945caa8dafb0267f152d7e3c3
Mike Lyncheski <mll@harriergroup.com>
2019-10-18 17:27:42 -0400

    Started with standard 3.1 template and then followed Tim Corey video
    https://www.youtube.com/watch?v=CaxR4_fP-FA

---
+++ BlazorDemoTim/Server/Controllers/PersonController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorDemoTim.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BlazorDemoTim.Server.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class PersonController : ControllerBase
+    {
+		[HttpGet]
+		[Route("all")]
+		public IEnumerable<PersonModel> GetPeople()
+		{
+			return new List<PersonModel> {
+				new PersonModel
+				{
+					FirstName = "Mike",
+					LastName = "Lyncheski",
+					AccountBalance = -48.17M
+				},
+				new PersonModel
+				{
+					FirstName = "Kristyn",
+					LastName = "Lyncheski",
+					AccountBalance = 8.17M
+				},
+				new PersonModel
+				{
+					FirstName = "Ann Marie",
+					LastName = "Rillo",
+					AccountBalance = 148.17M
+				}
+			};
+		}
+
+		[HttpPost]
+		public void Post(PersonModel person)
+		{
+
+		}
+	}
+}
---
+++ BlazorDemoTim/Shared/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorDemoTim.Shared
+{
+	public class PersonModel
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public decimal AccountBalance { get; set; }
+		public string FullName => $"{FirstName} {LastName}";
+	}
+}

f53bccb8a63bfda36ea5b768e523922e6fdfbcb2
Mike Lyncheski <mll@harriergroup.com>
2019-10-19 18:15:37 -0400

    Code behind file

---
+++ BlazorDemoTim/Client/Pages/FetchData.razor.cs
@@ -0,0 +1,36 @@
+﻿using BlazorDemoTim.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+
+//@inherits FetchDataBase
+//@page "/fetchdata"
+//@using WebApplication33.Data
+//@inject WeatherForecastService ForecastService  https://www.telerik.com/blogs/using-a-code-behind-approach-to-blazor-components
+
+// Partial class support is coming https://github.com/aspnet/AspNetCore/issues/5487
+
+
+namespace BlazorDemoTim.Client.Pages
+{
+	public class FetchDataBase : ComponentBase
+	{
+		public WeatherForecast[] forecasts;
+
+		[Inject]
+		public HttpClient Http { get; set; }
+
+		protected override async Task OnInitializedAsync()
+		{
+			Console.WriteLine("Fetching data ...");
+
+			forecasts = await Http.GetJsonAsync<WeatherForecast[]>("WeatherForecast");
+
+			// Projects now use System.Text.Json by default for JSON
+			Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(forecasts));
+		}
+	}
+}

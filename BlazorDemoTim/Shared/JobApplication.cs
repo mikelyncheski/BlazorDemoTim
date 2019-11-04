6cbb58caa9fe0c05b3305ace86855f8b5f29828d
Mike Lyncheski <mll@harriergroup.com>
2019-11-03 20:46:02 -0500

    Nick Chapsas Youtube - Creating Forms with Validation
    https://youtu.be/zfqQ_fhmPOQ

---
+++ BlazorDemoTim/Shared/JobApplication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BlazorDemoTim.Shared
+{
+	public class JobApplication
+	{
+		[Required]
+		public string Name { get; set; }
+		[StringLength(50, ErrorMessage = "Too much.")]
+		public string Description { get; set; }
+		[Required]
+		[Range(10000, 150000, ErrorMessage = "> 10K and < 150K") ]
+		public int Salary { get; set; }
+		[Required]
+		public bool DoesOpenSource { get; set; }
+		[Required]
+		public DateTime AvailableDate { get; set; }
+	}
+}

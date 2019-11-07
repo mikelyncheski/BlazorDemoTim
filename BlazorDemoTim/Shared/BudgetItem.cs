410474821426cc2079f525df617b477d1c4ebade
Mike Lyncheski <mll@harriergroup.com>
2019-11-06 19:48:44 -0500

    Bonus Sliders from Steve Sanderson Demo
    https://youtu.be/6BT2AF9PO5g

---
+++ BlazorDemoTim/Shared/BudgetItem.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorDemoTim.Shared
+{
+	public class BudgetItem
+	{
+		public string Name { get; set; }
+		public decimal Amount { get; set; }
+
+		public BudgetItem(string name)
+		{
+			Name = name;
+		}
+	}
+}
